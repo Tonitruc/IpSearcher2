d876a056ce754fb9fa235569b0477663abc567d6
Tonitruc <gromahacer@gmail.com>
2024-02-26 23:51:24 +0300

    update

--- src/main/java/org/example/ipsearcher/service/IpService.java
+++ src/main/java/org/example/ipsearcher/service/IpService.java
@@ -6,6 +6,7 @@ import org.example.ipsearcher.model.IpEntity;
 import org.example.ipsearcher.repository.IpRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.regex.Pattern;
 
@@ -19,8 +20,10 @@ public class IpService {
         if(!isValidIp(ip))
             throw new IllegalArgumentException("Invalid IP address");
 
-        String url = "http://ip-api.com/json/" + ip;
-        IpResponse response = restTemplate.getForObject(url, IpResponse.class);
+        String baseUrl = "http://ip-api.com/json/";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .path(ip);
+        IpResponse response = restTemplate.getForObject(builder.toUriString(), IpResponse.class);
 
         if (response.getCountry() != null && response.getRegionName() != null && response.getCity() != null) {
             ipRepository.save(new IpEntity(response.getQuery(), response.getCountry(),
